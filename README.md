a06bb0e89e17a83af3de56ab82c0a2d5fe377584
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-12-15 11:14:46 -0500

    c and make files

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # pico-build
+
+
+This repo is the source code, and provides instructions, for building the .elf file that is loaded to the RP2040 chip on the Pioreactor hat.
+
+
+### Building
+
+ - on a Raspberry Pi, follow the setup instructions in Chapter 1. Quick Pico Setup of [the datasheet](https://datasheets.raspberrypi.com/pico/getting-started-with-pico.pdf).
+ - `mkdir ~/pico/pico-examples/pioreactor && cd ~/pico/pico-examples/pioreactor`
+ - Move `main.c` and `CMakeLists.txt` from this repo to the cwd
+ - `cp ../../pico-sdk/external/pico_sdk_import.cmake .`
+ - `mkdir pioreactor/build && cd build`
+ - `export PICO_SDK_PATH=../../../pico-sdk`
+ - `cmake .. && make`
+ - If a RP2040 is connected over SWD, you can upload with `openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg -c "program main.elf verify reset exit"`
---
+++ main.c
@@ -0,0 +1,152 @@
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "hardware/irq.h"
+#include "hardware/adc.h"
+#include "hardware/pwm.h"
+
+/* The code implements the following i2c API
+1. Write to any of the first 4 addresses (0, 1, 2, 3) to set the duty cycle of the corresponding PWM
+2. Read from any of the last 4 addresses (4, 5, 6, 7) to get the 16 bit ADC value of the corresponding ADC
+
+Here are some examples of using i2cset and i2cget to interact with this program:
+
+To set the duty cycle of the PWM channel corresponding to address 0 to 75%, you could use the following i2cset command:
+
+> i2cset -y 1 0x30 0 0xC0
+
+To read the 16-bit ADC value of the ADC channel corresponding to address 4, you could use the following i2cget command:
+
+> i2cget -y 1 0x30 w 4
+
+Note that the -y flag is used to automatically answer yes to any prompt from i2cset or i2cget. 
+The 1 after the -y flag specifies the i2c bus number to use, and the 0x30 specifies the i2c address of the peripheral. 
+The 0 and w in the i2cset and i2cget commands, respectively, specify the starting register address to be accessed. 
+The 4 in the i2cget command specifies the address of the ADC channel to read.
+*/
+
+// define I2C addresses to be used for this peripheral
+#define I2C1_PERIPHERAL_ADDR 0x30
+
+// GPIO pins to use for I2C
+#define GPIO_SDA0 6
+#define GPIO_SCK0 7
+
+// GPIO pins to use for PWM -> LED channels
+#define LED_CHANNEL_A_PIN 15 // TODO: fix me
+#define LED_CHANNEL_B_PIN 17 // TODO: fix me
+#define LED_CHANNEL_C_PIN 16
+#define LED_CHANNEL_D_PIN 17
+
+// first 4 addresses are for the PWM channels for LEDS  (A, B, C, D)
+// second 4 addresses are for the ADCs (0, 1, 2, 3)
+uint8_t pointer = 0;
+
+// store the GPIO pins of the PWMs 
+uint8_t pwm_channels[4] = {LED_CHANNEL_A_PIN, LED_CHANNEL_B_PIN, LED_CHANNEL_C_PIN, LED_CHANNEL_D_PIN};
+
+// store the duty cycles of the PWMs
+uint8_t pwm_duty_cycles[4];
+
+
+void set_up_pwm_pin(uint pin) { 
+    // starts at duty_cycle = 0, hz = 125M
+    gpio_set_function(pin, GPIO_FUNC_PWM);
+    uint slice_num = pwm_gpio_to_slice_num(pin);
+    pwm_config config = pwm_get_default_config();
+    pwm_init(slice_num, &config, true); 
+    pwm_set_gpio_level(pin, 0);
+};
+
+void i2c1_irq_handler() {
+
+    // Get interrupt status
+    uint32_t status = i2c1->hw->intr_stat;
+
+    // Check to see if we have received data from the I2C controller
+    if (status & I2C_IC_INTR_STAT_R_RX_FULL_BITS) {
+
+        // Read the data (this will clear the interrupt)
+        uint32_t value = i2c1->hw->data_cmd;
+
+        // Check if this is the 1st byte we have received
+        if (value & I2C_IC_DATA_CMD_FIRST_DATA_BYTE_BITS) {
+
+            // If so treat it as the address to use
+            pointer = (uint8_t)(value & I2C_IC_DATA_CMD_DAT_BITS);
+
+        } else {
+            if (pointer <= 3){
+                // If not 1st byte then store the data in the RAM
+                // and increment the address to point to next byte
+                pwm_duty_cycles[pointer] = (uint8_t)(value & I2C_IC_DATA_CMD_DAT_BITS);
+                pwm_set_gpio_level(pwm_channels[pointer], ((uint16_t) pwm_duty_cycles[pointer]) << 8);
+            }
+        }
+    }
+
+    // Check to see if the I2C controller is requesting data from the RAM
+    if (status & I2C_IC_INTR_STAT_R_RD_REQ_BITS) {
+        if (pointer >= 4){
+            uint8_t adc_input = pointer - 4;
+            adc_select_input(adc_input);
+            uint16_t raw = adc_read();
+            i2c1->hw->data_cmd = (raw & 0xFF); // Send the low-order byte
+            i2c1->hw->data_cmd = (raw >> 8);   // Send the high-order byte
+        } else{
+            i2c1->hw->data_cmd = 0; // return 0 if not ADC
+        }
+
+        // Clear the interrupt
+        i2c1->hw->clr_rd_req;
+    }
+}
+
+
+// Main loop - initilises system and then loops while interrupts get on with processing the data
+int main() {
+
+    // setup ADC
+    stdio_init_all(); 
+
+    // Initialise ADCs
+    adc_init();
+    // Make sure GPIO is high-impedance, no pullups etc
+    adc_gpio_init(26);
+    adc_gpio_init(27);
+    adc_gpio_init(28);
+    adc_gpio_init(29);
+
+    // Select ADC input 0 initially (GPIO26)
+    adc_select_input(0);
+
+    // Setup I2C1 as peripheral
+    i2c_init(i2c1, 100000); 
+    i2c_set_slave_mode(i2c1, true, I2C1_PERIPHERAL_ADDR);
+
+    // Setup GPIO pins to use and add pull up resistors
+    gpio_set_function(GPIO_SDA0, GPIO_FUNC_I2C);
+    gpio_set_function(GPIO_SCK0, GPIO_FUNC_I2C);
+    gpio_pull_up(GPIO_SDA0);
+    gpio_pull_up(GPIO_SCK0);
+
+    // set up PWMs
+    set_up_pwm_pin(LED_CHANNEL_A_PIN);
+    set_up_pwm_pin(LED_CHANNEL_B_PIN);
+    set_up_pwm_pin(LED_CHANNEL_C_PIN);
+    set_up_pwm_pin(LED_CHANNEL_D_PIN);
+
+    // Enable the I2C interrupts we want to process
+    i2c1->hw->intr_mask = (I2C_IC_INTR_MASK_M_RD_REQ_BITS | I2C_IC_INTR_MASK_M_RX_FULL_BITS);
+
+    // Set up the interrupt handler to service I2C interrupts
+    irq_set_exclusive_handler(I2C1_IRQ, i2c1_irq_handler);
+
+    // Enable I2C interrupt
+    irq_set_enabled(I2C1_IRQ, true);
+
+    // Do nothing in main loop
+    while (true) {
+        tight_loop_contents();
+    }
+    return 0;
+}
